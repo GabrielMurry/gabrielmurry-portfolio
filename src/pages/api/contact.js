43e167fab5020ee635ab1e48c9da59c6b3f99c1c
Gabriel <gabrielmurryedh@gmail.com>
2023-03-15 00:38:57 -0700

    slight changes to contact api

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -25,19 +25,33 @@ export default async function (req, res) {
     });
   });
 
+  // const mailData = {
+  //   from: "gm.portfolio123@gmail.com",
+  //   to: "gabrielmurry.work@gmail.com",
+  //   subject: `Message From ${req.body.name}`,
+  //   text:
+  //     req.body.subject +
+  //     " | Message: " +
+  //     req.body.message +
+  //     " | Sent from: " +
+  //     req.body.email,
+  //   html: `${req.body.subject ? `<p>Subject: ${req.body.subject}</p>` : ""}
+  //   <div>${req.body.message}</div>
+  //   <p>Sent from: ${req.body.email}</p>`,
+  // };
+
+  const { name, email, subject, message } = req.body;
+
   const mailData = {
-    from: "gm.portfolio123@gmail.com",
+    from: {
+      name: `${name}`,
+      address: "gm.portfolio123@gmail.com",
+    },
+    replyTo: email,
     to: "gabrielmurry.work@gmail.com",
-    subject: `Message From ${req.body.name}`,
-    text:
-      req.body.subject +
-      " | Message: " +
-      req.body.message +
-      " | Sent from: " +
-      req.body.email,
-    html: `${req.body.subject ? `<p>Subject: ${req.body.subject}</p>` : ""}
-    <div>${req.body.message}</div>
-    <p>Sent from: ${req.body.email}</p>`,
+    subject: subject,
+    text: message,
+    html: `${message}`,
   };
 
   await new Promise((resolve, reject) => {
