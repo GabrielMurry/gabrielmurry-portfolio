2cc6da5a7b5cb96ece5d52130d7266bb65e994c8
Gabriel <gabrielmurryedh@gmail.com>
2023-03-15 00:45:33 -0700

    more minor fixes to contact api

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,7 +1,90 @@
-require("dotenv").config();
-let nodemailer = require("nodemailer");
+// require("dotenv").config();
+// let nodemailer = require("nodemailer");
 
-export default async function (req, res) {
+// export default async function (req, res) {
+//   const transporter = nodemailer.createTransport({
+//     port: 465,
+//     host: "smtp.gmail.com",
+//     auth: {
+//       user: "gm.portfolio123@gmail.com",
+//       pass: `${process.env.GMAIL_APP_PASSWORD}`,
+//     },
+//     secure: true,
+//   });
+
+//   await new Promise((resolve, reject) => {
+//     // verify connection config
+//     transporter.verify(function (error, success) {
+//       if (error) {
+//         console.log(error);
+//         reject(error);
+//       } else {
+//         console.log("Server is ready to take our messages");
+//         resolve(success);
+//       }
+//     });
+//   });
+
+//   // const mailData = {
+//   //   from: "gm.portfolio123@gmail.com",
+//   //   to: "gabrielmurry.work@gmail.com",
+//   //   subject: `Message From ${req.body.name}`,
+//   //   text:
+//   //     req.body.subject +
+//   //     " | Message: " +
+//   //     req.body.message +
+//   //     " | Sent from: " +
+//   //     req.body.email,
+//   //   html: `${req.body.subject ? `<p>Subject: ${req.body.subject}</p>` : ""}
+//   //   <div>${req.body.message}</div>
+//   //   <p>Sent from: ${req.body.email}</p>`,
+//   // };
+
+//   const { name, email, subject, message } = req.body;
+
+//   const mailData = {
+//     from: {
+//       name: `${name}`,
+//       address: "gm.portfolio123@gmail.com",
+//     },
+//     replyTo: email,
+//     to: "gabrielmurry.work@gmail.com",
+//     subject: subject,
+//     text: message,
+//     html: `${message}`,
+//   };
+
+//   await new Promise((resolve, reject) => {
+//     // send mail
+//     transporter.sendMail(mailData, (err, info) => {
+//       if (err) {
+//         console.error(err);
+//         reject(err);
+//       } else {
+//         console.log(info);
+//         resolve(info);
+//       }
+//     });
+//   });
+
+//   // transporter.sendMail(mailData, function (err, info) {
+//   //   if (err) {
+//   //     console.log(err);
+//   //   } else {
+//   //     console.log(info);
+//   //   }
+//   // });
+
+//   res.status(200).json({ status: "OK" });
+//   // res.send();
+// }
+
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+
+export default async (req, res) => {
+  const { name, email, subject, message } = req.body;
+
   const transporter = nodemailer.createTransport({
     port: 465,
     host: "smtp.gmail.com",
@@ -13,7 +96,7 @@ export default async function (req, res) {
   });
 
   await new Promise((resolve, reject) => {
-    // verify connection config
+    // verify connection configuration
     transporter.verify(function (error, success) {
       if (error) {
         console.log(error);
@@ -24,23 +107,6 @@ export default async function (req, res) {
       }
     });
   });
-
-  // const mailData = {
-  //   from: "gm.portfolio123@gmail.com",
-  //   to: "gabrielmurry.work@gmail.com",
-  //   subject: `Message From ${req.body.name}`,
-  //   text:
-  //     req.body.subject +
-  //     " | Message: " +
-  //     req.body.message +
-  //     " | Sent from: " +
-  //     req.body.email,
-  //   html: `${req.body.subject ? `<p>Subject: ${req.body.subject}</p>` : ""}
-  //   <div>${req.body.message}</div>
-  //   <p>Sent from: ${req.body.email}</p>`,
-  // };
-
-  const { name, email, subject, message } = req.body;
 
   const mailData = {
     from: {
@@ -67,14 +133,5 @@ export default async function (req, res) {
     });
   });
 
-  // transporter.sendMail(mailData, function (err, info) {
-  //   if (err) {
-  //     console.log(err);
-  //   } else {
-  //     console.log(info);
-  //   }
-  // });
-
   res.status(200).json({ status: "OK" });
-  // res.send();
-}
+};
