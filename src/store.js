249f2ae6697203bbc00eb4d462711f8bf47cdbbe
Gabriel <gabrielmurryedh@gmail.com>
2023-10-15 13:30:12 -0700

    Versioning untracked files

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import inViewSlice from "./slices/inViewSlice";
-
-export const store = configureStore({
-  reducer: {
-    inView: inViewSlice,
-  },
-});
